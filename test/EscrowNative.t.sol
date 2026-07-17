d5f3dd2d5f129f9f58a48c7523bfe400dd4599cc
Nima Rasooli <me@nimara.xyz>
2026-07-17 04:20:15 +0200

    fix: check proof newer than start block

--- test/EscrowNative.t.sol
+++ test/EscrowNative.t.sol
@@ -151,6 +151,7 @@ contract EscrowNativeTest is Test {
 
         assertEq(escrow.bondedExecutor(), executor);
         assertEq(escrow.executionDeadline(), block.timestamp + 5 minutes);
+        assertEq(escrow.bondStartBlock(), block.number);
         assertTrue(escrow.is_bonded());
         assertEq(escrow.bondPot(), 0);
         assertEq(address(escrow).balance, REWARD_AMOUNT + PAYMENT_AMOUNT);
@@ -220,8 +221,22 @@ contract EscrowNativeTest is Test {
 
     function testCollectNativeRequiresProof() public {
         _bondExecutor();
+
+        uint256 proofBlock = block.number + 1;
+        vm.roll(proofBlock + 1);
+        vm.setBlockhash(proofBlock, bytes32(uint256(1)));
+
         vm.prank(executor);
         vm.expectRevert();
+        escrow.collect(_dummyProof(), proofBlock);
+    }
+
+    function testCollectNativeRejectsProofBeforeBond() public {
+        vm.roll(100);
+        _bondExecutor();
+
+        vm.prank(executor);
+        vm.expectRevert(EscrowBase.ProofBeforeBond.selector);
         escrow.collect(_dummyProof(), block.number - 1);
     }
 
